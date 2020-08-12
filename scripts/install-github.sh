9149cf7b98ea8103b33d34ef1d2d07db35227200
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-12 00:33:08 -0400

    Remove GitHub script

--- scripts/install-github.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu -o pipefail
-
-# FIXME Move this into main koopa Bash functions.
-
-python3 -m pip install \
-    --upgrade \
-    'https://github.com/acidgenomics/koopa/archive/python.tar.gz'
