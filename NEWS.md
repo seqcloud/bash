c291cb9482cf771df2dcc3fa27772d12ab695142
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-09 11:31:04 -0500

    Update package

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# koopa 0.0.3 (2020-12-09)
+
+- Updated Python dependency from 3.7 to 3.8.
+- Improved downloading of Ensembl transcriptomes. Now including the cDNA and
+  ncRNA annotations in a merged file. See recommendations in
+  [nf-core/rnaseq issue 527](https://github.com/nf-core/rnaseq/issues/527).
+
 # koopa 0.0.2 (2020-11-10)
 
 - Updated Python dependency to 3.9.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 koopa Python package.
-Updated 2020-11-11.
+Updated 2020-12-09.
 
 See also:
 - https://packaging.python.org/tutorials/packaging-projects/
@@ -11,12 +11,12 @@ import setuptools
 
 setuptools.setup(
     name="koopa",
-    version="0.0.2",
+    version="0.0.3",
     author="Michael Steinbaugh",
     author_email="mike@steinbaugh.com",
     description="Shell bootloader for bioinformatics.",
     url="https://koopa.acidgenomics.com/",
     packages=setuptools.find_packages(),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=["flake8", "pylint", "pytest", "six"],
 )
