363e75893bdf796a732771e3191212e17be1a94c
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-09 14:18:16 -0500

    Draft update

--- koopa/genome/ensembl.py
+++ koopa/genome/ensembl.py
@@ -4,11 +4,11 @@ Ensembl genome utilities.
 """
 
 from os.path import join
+from shutil import copyfileobj
 
 from koopa.genome import _genome_version
 from koopa.files import download
 from koopa.strings import paste_url
-from shutil import copyfileobj
 
 
 def download_ensembl_genome(
@@ -34,8 +34,6 @@ def download_ensembl_genome(
     download(url=fasta_url, output_dir=output_dir, decompress=decompress)
 
 
-# FIXME Need to combine cDNA and ncDNA files here.
-# FIXME SAVE TO SUBDIRECTORIES THEN COMBINE.
 def download_ensembl_transcriptome(
     organism, build, release_url, output_dir, decompress
 ):
@@ -46,7 +44,7 @@ def download_ensembl_transcriptome(
     output_dir = join(output_dir, "transcriptome")
     transcriptome_file = join(output_dir, "transcriptome.fa.gz")
     base_url = paste_url(release_url, "fasta", organism.lower())
-    # cDNA.
+    # cDNA FASTA.
     cdna_output_dir = join(output_dir, "cdna")
     cdna_base_url = paste_url(base_url, "cdna")
     cdna_readme_url = paste_url(cdna_base_url, "README")
@@ -54,7 +52,12 @@ def download_ensembl_transcriptome(
     cdna_fasta_file_basename = organism + "." + build + ".cdna.all.fa.gz"
     cdna_fasta_file = join(cdna_output_dir, cdna_fasta_file_basename)
     cdna_fasta_url = paste_url(cdna_base_url, cdna_fasta_file_basename)
-    # ncDNA.
+    download(url=cdna_readme_url, output_dir=cdna_output_dir)
+    download(url=cdna_checksums_url, output_dir=cdna_output_dir)
+    download(
+        url=cdna_fasta_url, output_file=cdna_fasta_file, decompress=decompress
+    )
+    # ncDNA FASTA.
     ncdna_output_dir = join(output_dir, "ncdna")
     ncdna_base_url = paste_url(base_url, "ncdna")
     ncdna_readme_url = paste_url(ncdna_base_url, "README")
@@ -62,28 +65,24 @@ def download_ensembl_transcriptome(
     ncdna_fasta_file_basename = organism + "." + build + ".ncdna.all.fa.gz"
     ncdna_fasta_file = join(ncdna_output_dir, ncdna_fasta_file_basename)
     ncdna_fasta_url = paste_url(ncdna_base_url, ncdna_fasta_file_basename)
-    # Download files.
-    download(url=cdna_readme_url, output_dir=cdna_output_dir)
     download(url=ncdna_readme_url, output_dir=ncdna_output_dir)
-    download(url=cdna_checksums_url, output_dir=cdna_output_dir)
     download(url=ncdna_checksums_url, output_dir=ncdna_output_dir)
-    download(url=cdna_fasta_url, output_file=cdna_fasta_file, decompress=decompress)
-    download(url=ncdna_fasta_url, output_file=ncdna_fasta_file, decompress=decompress)
+    download(
+        url=ncdna_fasta_url,
+        output_file=ncdna_fasta_file,
+        decompress=decompress,
+    )
+    # Merged transcriptome FASTA.
     # Combine cDNA and ncDNA into a single merged FASTA. This method is memory
     # efficient. It automatically reads the input files chunk by chunk for you,
     # which is more more efficient and reading the input files in and will work
     # even if some of the input files are too large to fit into memory.
     # - https://stackoverflow.com/a/18209002
     # - https://stackoverflow.com/a/27077437
-    # FIXME MAKE THIS A NEW FUNCTION IN FILES.
-    # > with open(..., 'wb') as wfp:
-    # >     for fn in filenames:
-    # >         with open(fn, 'rb') as rfp:
-    # >             copyfileobj(rfp, wfp)
-    with open(transcriptome_file, 'wb') as wfd:
-        for f in [cdna_fasta_file, ncdna_fasta_file]:
-            with open(f, 'rb') as fd:
-                copyfileobj(fd, wfd)
+    with open(transcriptome_file, "wb") as output_file:
+        for file in [cdna_fasta_file, ncdna_fasta_file]:
+            with open(file, "rb") as file_open:
+                copyfileobj(file_open, output_file)
 
 
 def download_ensembl_gtf(
